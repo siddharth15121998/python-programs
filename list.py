5ca9f8fcc15195045a2c46800a1d72fa34c2e6c0
siddharth15121998 <mastersiddharthgupta@gmail.com>
2021-07-21 12:07:33 +0530

    python dictlisttuple

--- list.py
+++ list.py
@@ -1,8 +1,11 @@
-names=["siddharth","akshay","tamilarasi","shikhar",23,23.5,100]
-del names[3]
-names.remove("akshay")
-print(names)
-names.pop(1)
-print(names)
-names.clear()
-print(names)
+names=["siddharth","akshay","tamilarasi","aarohi"]
+identi=["abc"]
+# del names[3]
+# names.remove("akshay")
+# print(names)
+# names.pop(1)
+# print(names)
+# names.clear()
+# print(names)
+#print(max(names))
+# print(tuple(names))
