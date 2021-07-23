707f26e93604660ba4f87f01597177ed2268fdbd
siddharth15121998 <mastersiddharthgupta@gmail.com>
2021-07-23 10:51:51 +0530

    moduleconcept

---
+++ day5tasks/addModule.py
@@ -0,0 +1,5 @@
+def addNum1(a,b):
+    return a+b
+
+def sayHello():
+    return "Hello all"
---
+++ day5tasks/addnum.py
@@ -0,0 +1,6 @@
+import addModule as add
+n1=int(input("Enter a number: "))
+n2=int(input("Enter a number: "))
+print(add.addNum1(n1,n2))
+print(add.sayHello())
+#print(addnum1(n1,n2))
