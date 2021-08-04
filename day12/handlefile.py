1c6c02eb902c743828cd9875081cbe60780ef2dc
siddharth15121998 <mastersiddharthgupta@gmail.com>
2021-08-04 14:07:38 +0530

    filemenudriven

--- day12/handlefile.py
+++ day12/handlefile.py
@@ -1,4 +1,5 @@
 import logging
+import time
 myfile=open('namefile.txt','w')
 
 logging.basicConfig(filename="demolog1.log",level=logging.DEBUG)
@@ -9,18 +10,21 @@ while (True):
     print("2. View names")
     print("3. Exit")
     try:
-        choice=int(input("Enter  choice"))
+        choice=int(input("Enter  choice "))
         logging.info("User enterd correct choice")
     except:
         logging.error("something went wrong")
         continue
 
     if choice==1:
-        name=input("enter  name :")
+        n=input("enter  name :")
         myfile=open("namefile.txt",'a')
-        myfile.write(name+"\n")
+        myfile.write(n+" ")
+        current_time=time.localtime()
+        current_time_format=time.strftime("%H:%M:%S",current_time)
+        print("file updated on ",current_time_format)
     if choice==2:
-        print("\nDisplaying  names ")
+        print("Displaying names in files ")
         myfile=open('namefile.txt','r')
         x =myfile.read()
         print(str(x))
